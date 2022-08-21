4c8e97b19e1c8198efe5a91a4f85e1d657b15a95
Johnny Marcelino <johnnyg_engenheiro@hotmail.com>
2022-08-21 09:33:51 -0300

    @johnnymarcelino [Project Polimorfism updated] {09:33 21/08/2022}

--- Polimorfismo01/Polimorfismo01/PolimorGeom/Program.cs
+++ Polimorfismo01/Polimorfismo01/PolimorGeom/Program.cs
@@ -2,13 +2,12 @@
 using System;
 
 namespace Polimorfismo_Zoo.PolimorGeom {
-    public class Program : {
-        static void Main(string[] args) {
+    public class Program {
+        static void With(string[] args) {
             Console.WriteLine("What is the hight of the square: ");
             double hight_square = Convert.ToDouble(Console.ReadLine());
             Square square = new Square(4, hight_square, hight_square);
             Console.WriteLine($"The total area of a square with a hight and base {square.Hight} is: {square.TotalArea()}");
-            Console.ReadKey();
 
             Console.WriteLine("Type the hight of the triangle: ");
             double hight_triangle = Convert.ToDouble(Console.ReadLine());
@@ -16,7 +15,6 @@ namespace Polimorfismo_Zoo.PolimorGeom {
             double baseTriangle = Convert.ToDouble(Console.ReadLine());
             Triangle triangle = new Triangle(3, hight_triangle, baseTriangle);
             Console.WriteLine($"The total area of a triangle with a hight {triangle.Hight} and base {triangle.Base} is: {triangle.TotalArea()}");
-            Console.ReadKey();
 
             Console.WriteLine("Type the hight of the rectangle: ");
             double hight_rectangle = Convert.ToDouble(Console.ReadLine());
@@ -24,7 +22,6 @@ namespace Polimorfismo_Zoo.PolimorGeom {
             double baseRectangle = Convert.ToDouble(Console.ReadLine());
             Rectangle retangulo = new Rectangle(4, baseRectangle, baseRectangle);
             Console.WriteLine($"The total area of a rectangle with a hight {retangulo.Hight} and base {retangulo.Base} is: {retangulo.TotalArea()}");
-            Console.ReadKey();
         }
     }
 }
